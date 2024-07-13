4cc368c12bb145947ad7ed38a81ff449ab5f66a4
Ignat Sychev <amanewave@gmail.com>
2024-07-13 15:28:26 +1000

    feat:up

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
+const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-
+app.use(express.json())
+app.use("/auth", authRouter)
+app.use(cors())
 const start = async() => {
     try {
         await mongoose.connect(
-					`mongodb+srv://amanewave:<password>@notio.eys9cxf.mongodb.net/?retryWrites=true&w=majority&appName=notio`
+					`mongodb+srv://amanewave:G9hBsZYokKAVTDO7@notio.eys9cxf.mongodb.net/?retryWrites=true&w=majority&appName=notio`
 				)
         app.listen(PORT, () => console.log(`server started on ${PORT} port`))
     } catch (e) {
